0ddd7724f37d0c815d025531b056ec10e9f1fa2a
amh1k <abdulmoizx97@gmail.com>
2026-05-15 19:55:35 +0500

    Add templates, static files, and enhanced handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
 
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network address block")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	infoLog.Printf("Starting server on %s", *addr)
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
