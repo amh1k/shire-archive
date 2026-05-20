9fb2649ceaf8e30f715f14b59d2553084a727ebe
amh1k <abdulmoizx97@gmail.com>
2026-05-20 23:32:31 +0500

    feat: implement user registration and authentication flow
    - Add bcrypt for secure password hashing and verification
    - Implement UserModel.Insert to handle user registration and duplicate email checks
    - Implement UserModel.Authenticate for validating login credentials
    - Create login handlers (userLogin, userLoginPost) and userLoginForm struct
    - Complete userSignupPost handler to redirect users upon successful sign-up
    - Enhance validator logic to support NonFieldErrors for generic form errors
    - Add login.tmpl HTML template for the login page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,6 +22,11 @@ type userSignupForm struct {
 	Password string `form:"password"`
 	validator.Validator `form:"-"`
 }
+type userLoginForm struct {
+	Email string `form:"email"`
+	Password string `form:"password"`
+	validator.Validator `form:"-"`
+}
 
 
 
@@ -163,14 +168,67 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
 		return
 	}
-	fmt.Fprintln(w, "Create a new user...")
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "Email address is already in use")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+	} else {
+		app.serverError(w, err)
+	}
+	return 
+	}
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+
+
 
 
 }
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = userLoginForm{}
+	app.render(w, http.StatusOK, "login.tmpl", data)
+
 
 }
 func (app *application)userLoginPost(w http.ResponseWriter, r *http.Request) {
+	var form userLoginForm
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This fieldmust be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	if (!form.Valid()) {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "login.tmpl", data)
+		return
+	}
+	id, err := app.users.Authenticate(form.Email, form.Password)
+	if err !=  nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.AddNonFieldError("Email or password is incorrect")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "login.tmpl", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	err = app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
+	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 
 }
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
