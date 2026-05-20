7a3543af7c900e771f6a817e50d71ac42797dd96
amh1k <abdulmoizx97@gmail.com>
2026-05-20 14:01:07 +0500

    feat: add user signup routing, handlers, and validation
    - Introduce routes and skeleton handlers for user signup, login, and logout
    - Implement form parsing and validation logic for `userSignupPost`
    - Extend the validator package with `EmailRX`, `Matches`, and `MinChars`
    - Define new domain errors: `ErrInvalidCredentials` and `ErrDuplicateEmail`
    - Integrate `UserModel` into the main application struct
    - Update navigation UI to include Signup, Login, and Logout actions
    - Add initial `users.go` model and `signup.tmpl` view

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,6 +22,7 @@ type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
 	snippets *models.SnippetModel
+	users *models.UserModel
 	templateCache map[string] *template.Template
 	formDecoder *form.Decoder
 	sessionManager scs.SessionManager
@@ -49,9 +50,11 @@ func main() {
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &models.SnippetModel{DB: db},
+		users : &models.UserModel{DB: db},
 		templateCache: templateCache,
 		formDecoder: formDecoder,
 		sessionManager: *sessionManager,
+
 		
 		
 	}
